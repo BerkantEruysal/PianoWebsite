825c6b6a0fb548f583eb78852bd6ac5a8fc7b8dd
ArwensChild <75691062+BerkantEruysal@users.noreply.github.com>
2023-05-06 18:29:43 +0300

    Final sound part

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,33 @@
-document.addEventListener('keydown', (event) => {
-    var audio = new Audio('/src/static/notes/448532__tedagame__c5.ogg');
-    audio.play();
-    console.log(event)
-})
+import soundEvents from "./sounds.js";
+
+let counter = 0;
+var origin = 30
+let listenedKeys = "q2we4r5ty7u8"
+
+function onKeyDown(e){
+    if(e.repeat){return}
+    const keyIndex = listenedKeys.indexOf(e.key.toLowerCase());
+    if(keyIndex != -1){
+        const events = soundEvents[Object.keys(soundEvents)[origin + keyIndex]];
+        document.dispatchEvent(events.playEvent)
+    }
+    
+}
+
+function onKeyUp(e){
+    console.log(e.key)
+    const keyIndex = listenedKeys.indexOf(e.key.toLowerCase());
+    if(keyIndex != -1){
+        const events = soundEvents[Object.keys(soundEvents)[origin + keyIndex]];
+        document.dispatchEvent(events.stopEvent)
+    }
+}
+
+document.addEventListener('keydown', onKeyDown)
+
+document.addEventListener('keyup',  onKeyUp)
+
+
+
+
+
